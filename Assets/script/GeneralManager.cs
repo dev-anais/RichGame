116e4ffef0e2e3ba8e54cb13242825bcf1142fcc
yuneunhye <anaisleh33@gmail.com>
2021-01-25 19:48:38 +0900

    게임화면에서 설정페이지 작업
    게임화면에서 설정페이지 작업

---
+++ Assets/script/GeneralManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GeneralManager : MonoBehaviour
+{
+    public GameObject menuSet;
+
+    // 게임 처음 시작할 때 
+    public void  MoveToGame(){
+       SceneManager.LoadScene("InitialGame");
+    }
+
+    public void  HideSettingDialog(){
+        menuSet.SetActive(false);
+    }
+    //세팅 화면 보여주기
+    public void ShowSettingDialog(){
+        menuSet.SetActive(true);
+    }
+}
